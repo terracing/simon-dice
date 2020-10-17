f750489a246556827f7828e97721836f731f3843
Omar Terrazas <terrazas.omar@gmail.com>
2020-10-16 23:32:01 -0500

    Se ilumina secuencia.

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -9,6 +9,9 @@ const btnFour = document.getElementById('four')
 class Game {
     constructor() {
         this.inicializar()
+        this.generateSecuence()
+        this.nextLevel()
+        this.secuence
     }
 
     inicializar() {
@@ -22,8 +25,41 @@ class Game {
         }
     }
 
-    generarSecuencia() {
-        this.secuence = new Array(10).fill(0).map(n => Math.floor(Math.random * 4))
+    generateSecuence() {
+        this.secuence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    }
+
+    nextLevel() {
+        this.lightSecuence()
+    }
+
+    transformSecuence(secuenceNumber) {
+        switch(secuenceNumber) {
+            case 0 : 
+                return 'btnOne'
+            case 1:
+                return 'btnTwo'
+            case 2:
+                return 'btnThree'
+            case 3:
+                return 'btnFour'
+        }
+    }
+
+    lightColor(color) {
+        this.colors[color].classList.add('light')
+        setTimeout(() => this.darkenColor(color), 350)
+    }
+
+    darkenColor(color) {
+        this.colors[color].classList.remove('light')
+    }
+
+    lightSecuence() {
+        for (let i = 0; i < this.level; i++) {
+            const color = this.transformSecuence(this.secuence[i])
+            setTimeout(() => this.lightColor(color), 1000 * i)
+        }
     }
 }
 
